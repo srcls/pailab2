615778240fcc1c2834ca62a0945cb34d86c4f40f
Marcin Pietrzak <p.marcin84@gmail.com>
2020-05-04 03:14:29 +0200

    Add project files.

---
+++ pailab2/calculator.aspx
@@ -0,0 +1,29 @@
+﻿<%@ Page Language="vb" AutoEventWireup="false" CodeBehind="calculator.aspx.vb" Inherits="pailab2.calculator" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <asp:Label ID="Label1" runat="server" Text="a = "></asp:Label>
+        <asp:TextBox ID="a" runat="server"></asp:TextBox>
+        <p>
+            <asp:Label ID="Label2" runat="server" Text="b = "></asp:Label>
+            <asp:TextBox ID="b" runat="server"></asp:TextBox>
+        </p>
+        <p>
+            <asp:DropDownList ID="DropDownList1" runat="server">
+                <asp:ListItem Text="+" Value="1"></asp:ListItem>
+                <asp:ListItem Text="-" Value="2"></asp:ListItem>
+                <asp:ListItem Text="*" Value="3"></asp:ListItem>
+                <asp:ListItem Text="/" Value="4"></asp:ListItem>
+            </asp:DropDownList>
+        </p>
+        <asp:TextBox ID="result" runat="server"></asp:TextBox>
+        <asp:Button ID="Button1" runat="server" Text="Oblicz" />
+    </form>
+</body>
+</html>
---
+++ pailab2/calculator.aspx.vb
@@ -0,0 +1,36 @@
+﻿Public Class calculator
+    Inherits System.Web.UI.Page
+
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+
+    End Sub
+
+    Protected Sub a_TextChanged(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Protected Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        If String.IsNullOrEmpty(a.Text) Then
+            Exit Sub
+        End If
+        If IsNumeric(a.Text) And IsNumeric(b.Text) Then
+        Else
+            Exit Sub
+        End If
+
+        If DropDownList1.SelectedValue = "1" Then
+            result.Text = Integer.Parse(a.Text) + Integer.Parse(b.Text)
+        ElseIf DropDownList1.SelectedValue = "2" Then
+            result.Text = Integer.Parse(a.Text) - Integer.Parse(b.Text)
+        ElseIf DropDownList1.SelectedValue = "3" Then
+            result.Text = Integer.Parse(a.Text) * Integer.Parse(b.Text)
+        ElseIf DropDownList1.SelectedValue = "4" Then
+            result.Text = Integer.Parse(a.Text) / Integer.Parse(b.Text)
+        End If
+
+    End Sub
+
+    Protected Sub DropDownList1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles DropDownList1.SelectedIndexChanged
+
+    End Sub
+End Class
